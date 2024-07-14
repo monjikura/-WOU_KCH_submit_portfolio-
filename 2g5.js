123fd2e34d4775d4c04b89a53a31aa5b2fca5477
monjikura <68017784+monjikura@users.noreply.github.com>
2024-07-14 16:10:49 +0800

    Update

---
+++ 2g5.js
@@ -0,0 +1,30 @@
+// for Knight's tour
+ // problem using Warnsdorff's algorithm
+
+ class girl_pee {
+
+     standing( x , y ) {
+
+         that.y != y;
+         that.x != x; // false xor
+     }
+
+ }
+
+ girl_pee[883];
+ // Turing testing mod asm ( mov ) load
+ ;04 , 01 === null;
+ // function restricts
+ function prove( x , y ) {
+
+     return ((x >= 0 && y >= 0) &&
+             (x < N && y < N));
+
+ }
+
+ return girl_pee;
+
+ console.log.apply("null, [array]")
+
+ // " .... " switch .js console.log
+ // Call console.log with apply
